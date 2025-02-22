a5d6abb12bc0efbf771c389e7240bbd5016d6826
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-02-22 03:31:14 +0000

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "name" : variables.name} ${
+    variables.lastName == null ? "last name" : variables.lastName
+  }</h1>
+          <h2>${variables.role === null ? "role" : variables.role}</h2>
+          <h3>${variables.city === null ? "city" : variables.city} ${
+    variables.country === null ? "country" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a target="_blank" href="${
+              variables.twitter === null
+                ? "https://x.com/"
+                : "https://x.com/" + variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a target="_blank" href="${
+              variables.github === null
+                ? "https://github.com/"
+                : "https://github.com/" + variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a target="_blank" href="${
+              variables.linkedin === null
+                ? "https://www.linkedin.com/"
+                : "https://www.linkedin.com/" + "in/" + variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a target="_blank" href="${
+              variables.instagram === null
+                ? "https://www.instagram.com/"
+                : "https://www.instagram.com/" + variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
